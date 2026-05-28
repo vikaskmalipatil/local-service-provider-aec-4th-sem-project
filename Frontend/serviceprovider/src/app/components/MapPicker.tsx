a3877fd2341c870d47aabc0eb16e8f601f3f9aec
vikaskmalipatil <vikaskm7106@gmail.com>
2026-05-28 16:35:25 +0530

    added leaflet types

--- Frontend/serviceprovider/src/app/components/MapPicker.tsx
+++ Frontend/serviceprovider/src/app/components/MapPicker.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import L from "leaflet";
 
 // Fix marker icon
-delete L.Icon.Default.prototype._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
     iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
