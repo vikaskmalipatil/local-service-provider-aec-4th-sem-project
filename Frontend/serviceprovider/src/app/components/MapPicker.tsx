e69596e9a247c74c516b21f8c57b06ba75065458
vikaskmalipatil <vikaskm7106@gmail.com>
2026-05-28 16:54:36 +0530

    added leaflet types

--- Frontend/serviceprovider/src/app/components/MapPicker.tsx
+++ Frontend/serviceprovider/src/app/components/MapPicker.tsx
@@ -1,19 +1,30 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    useMapEvents,
+    useMap,
+} from "react-leaflet";
+
 import { useState, useEffect } from "react";
 import L from "leaflet";
 
-// Fix marker icon
+// Fix marker icon issue
 delete (L.Icon.Default.prototype as any)._getIconUrl;
+
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
-const defaultCenter = [13.3409, 74.7421];
+const defaultCenter: [number, number] = [13.3409, 74.7421];
 
 function FixMap() {
     const map = useMap();
@@ -27,40 +38,67 @@ function FixMap() {
     return null;
 }
 
-function LocationMarker({ onSelect, defaultLocation }: any) {
-    const [position, setPosition] = useState(defaultLocation || null);
+function LocationMarker({
+    onSelect,
+    defaultLocation,
+}: {
+    onSelect?: (location: { lat: number; lng: number }) => void;
+    defaultLocation?: [number, number] | null;
+}) {
+    const [position, setPosition] = useState<[number, number] | null>(
+        defaultLocation || null
+    );
 
     useMapEvents({
         click(e) {
             const { lat, lng } = e.latlng;
-            const pos = [lat, lng];
+            const pos: [number, number] = [lat, lng];
 
             setPosition(pos);
-            if (onSelect) onSelect({ lat, lng });
+
+            if (onSelect) {
+                onSelect({ lat, lng });
+            }
         },
     });
 
     useEffect(() => {
-        if (defaultLocation) setPosition(defaultLocation);
+        if (defaultLocation) {
+            setPosition(defaultLocation);
+        }
     }, [defaultLocation]);
 
     return position ? <Marker position={position} draggable /> : null;
 }
 
-export default function MapPicker({ onSelect, defaultLocation }) {
+export default function MapPicker({
+    onSelect,
+    defaultLocation,
+}: {
+    onSelect?: (location: { lat: number; lng: number }) => void;
+    defaultLocation?: [number, number] | null;
+}) {
     return (
-        <div style={{ zIndex: 10, position: 'relative' }}>
+        <div style={{ zIndex: 10, position: "relative" }}>
             <MapContainer
                 center={defaultLocation || defaultCenter}
                 zoom={13}
-                style={{ height: "300px", width: "100%", borderRadius: "0.5rem" }}
+                style={{
+                    height: "300px",
+                    width: "100%",
+                    borderRadius: "0.5rem",
+                }}
             >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
                 <FixMap />
-                <LocationMarker onSelect={onSelect} defaultLocation={defaultLocation} />
+
+                <LocationMarker
+                    onSelect={onSelect}
+                    defaultLocation={defaultLocation}
+                />
             </MapContainer>
         </div>
     );
